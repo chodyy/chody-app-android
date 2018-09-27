f2678515a0104d8e618c3e2eb6a2e1adc1f04cad
dgavio <b-aLengo7>
2018-09-27 15:09:48 +0200

    Creazione cartelle. Controllo stato di autenticazione dell'utente. Prime prove di creazione dei vari fragments.

--- app/src/main/java/com/chody/chody/MainActivity.java
+++ app/src/main/java/com/chody/chody/MainActivity.java
@@ -1,10 +1,13 @@
 package com.chody.chody;
 
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
+import com.chody.chody.fragments.CustomerFunctionFragment;
 import com.firebase.ui.auth.AuthUI;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -44,7 +47,6 @@ public class MainActivity extends AppCompatActivity {
         super.onStart();
         // Check if user is signed in (non-null) and update UI accordingly.
         FirebaseUser currentUser = mAuth.getCurrentUser();
-        //updateUI(currentUser);
     }
 
     private FirebaseUser getCurrentFirebaseUser() {
@@ -64,5 +66,14 @@ public class MainActivity extends AppCompatActivity {
                         .build(), RC_SIGN_IN);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(requestCode == RC_SIGN_IN){
+            if(resultCode == RESULT_OK) {
+                CustomerFunctionFragment customerFunctionFragment = new CustomerFunctionFragment();
+                getSupportFragmentManager().beginTransaction().add(R.id.placeholder, customerFunctionFragment).commit();
+            }
+        }
+    }
 
 }
